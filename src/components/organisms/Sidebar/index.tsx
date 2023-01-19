5093459372050091449719a6dd22e82fa6d3c661
SaniLubary <santiago.lp.cop@gmail.com>
2023-01-19 11:44:46 -0300

    feat: Add images and button to sidebar, update globals to use DM Sans font

--- src/components/organisms/Sidebar/index.tsx
+++ src/components/organisms/Sidebar/index.tsx
@@ -1,7 +1,9 @@
 import { Dispatch, SetStateAction, useState } from 'react'
 
-import { AiOutlineMenu } from 'react-icons/ai'
+import { AiOutlineClose } from 'react-icons/ai'
 import styles from './Sidebar.module.css'
+import Image from 'next/image'
+import Button from '@/components/atoms/Button'
 
 const menuItems = [
   { item: "inicio", page: null },
@@ -10,22 +12,21 @@ const menuItems = [
   { item: "contacto", page: null },
   { item: "blog", page: null },
   { item: "español", page: null },
-  { item: "img apple store", page: null },
-  { item: "img android", page: null },
-  { item: "Iniciar sesion", page: null },
-  { item: "Resgistrarse", page: null }
+  { item: "apple google stores", component: <Image width={200} height={130} src="/landing/appleGoogleStores.png" alt="Placeholder for apple and google store buttons" />, page: null },
+  { item: "iniciar sesion", component: <Button href='' size='small' text='Iniciar Sesion' key='iniciarSesion' />, page: null },
+  { item: "Resgistrarse", component: <Button href='' size='small' text='Registrarse' key='registro' />, page: null }
 ]
 
-function Sidebar({ setIsSidebarOpen }: { setIsSidebarOpen: Dispatch<SetStateAction<boolean>> }) {
+function Sidebar({ setIsSidebarOpen, isSidebarOpen }: { setIsSidebarOpen: Dispatch<SetStateAction<boolean>>, isSidebarOpen: boolean }) {
   const [selected, setSelected] = useState('inicio')
 
   return (
     <div className={styles.sidebar}>
-      <AiOutlineMenu onClick={() => setIsSidebarOpen(false)} style={{ cursor: 'pointer' }} size={30} />
+      <AiOutlineClose style={{ cursor: 'pointer' }} onClick={() => setIsSidebarOpen(!isSidebarOpen)} size={30} />
 
       <div className={styles.navigation}>
         {menuItems.map(item => <div className={item.item === selected ? styles.selected : undefined} onClick={() => setSelected(item.item)} key={item.item}>
-          {item.item}
+          {item.component || item.item}
         </div>)}
       </div>
     </div>
